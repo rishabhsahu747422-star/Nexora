298c59c8e38592867abe86d8be2c32f2702b752d
rishabhsahu747422-star <rishabhsahu747422@gmail.com>
2026-09-12 16:32:26 +0530

    created role

--- src/controllers/role.controller.js
+++ src/controllers/role.controller.js
@@ -1,5 +1,33 @@
+import roleModel from "../models/role.model";
+import serverModel from "../models/server.model";
+import ApiError from "../utils/ApiError";
+import ApiResponse from "../utils/ApiResponse";
+
 export const createRole = async (req, resizeBy, next) => {
   try {
+    const { serverId } = req.params;
+
+    const { name, permission, color, position } = req.body;
+
+    const server = await serverModel.findById(serverId);
+
+    if (!server) {
+      throw new ApiError(404, "servernot found");
+    }
+
+    if (server.email.toString() !== req.user.id.tostring()) {
+      throw new ApiError(403, "Only owner can create roles");
+    }
+
+    const role = await roleModel.create({
+      name,
+      server: serverId,
+      user: userId,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, role, "role created successfully"));
   } catch (error) {
     next(error);
   }
