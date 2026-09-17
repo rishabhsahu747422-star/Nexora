3dacf16b3a397574a2f82fb715803c0985f4a70a
rishabhsahu747422-star <rishabhsahu747422@gmail.com>
2026-09-17 22:22:31 +0530

    express validator

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,18 @@
+export const errorMiddleware = (err, req, res, next) => {
+  const statusCode =
+    err.statusCode ||
+    (err.name === "ValidationError" ? 400 : err.code === 1100 ? 409 : 500);
+  const errors =
+    err.name === "ValidationError"
+      ? Object.values(err.errors).map(({ path, message }) => ({
+          path,
+          message,
+        }))
+      : err.errors || [];
+
+  return res.status(statusCode).json({
+    success: false,
+    message: err.message || "Gadbad in error Middleware",
+    errors,
+  });
+};
---
+++ src/middlewares/validatte.middleware.js
@@ -0,0 +1,11 @@
+import { validationResult } from "express-validator";
+import ApiError from "../utils/ApiError.js";
+
+export const validate = (req, res, next) => {
+  const error = validationResult(req);
+
+  if (!error.isEmpty()) {
+    return next(new ApiError(400, "Vaidation error", error.array()));
+  }
+  next();
+};
